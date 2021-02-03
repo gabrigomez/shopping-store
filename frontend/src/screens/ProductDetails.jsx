71e9cafdcceff6322129977e48358e7ae98d35e8
Gabriel Gomes <gabrigomez15@gmail.com>
2021-02-03 11:54:44 -0300

    product details component init

---
+++ frontend/src/screens/ProductDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import axios from 'axios'
+
+const ProductDetails = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ProductDetails;
