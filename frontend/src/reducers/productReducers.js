87ecc1b8bbc50d0b91cf23d6f1c45d3e89b5fa9a
Gabriel Gomes <gabrigomez15@gmail.com>
2021-02-08 16:41:26 -0300

    fix redux in product details component

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,4 +1,11 @@
-import { PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants";
+import {
+    PRODUCT_DETAILS_FAIL,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_LIST_FAIL,
+    PRODUCT_LIST_REQUEST,
+    PRODUCT_LIST_SUCCESS
+} from "../constants/productConstants";
 
 export const productListReducer = (state = { loading: true, products: [] }, action) => {
     switch (action.type) {
@@ -18,7 +25,7 @@ export const productDetailsReducer = (state = { product: {}, loading: true }, ac
     switch (action.type) {
         case PRODUCT_DETAILS_REQUEST:
             return { loading: true }
-        case PRODUCT_LIST_SUCCESS:
+        case PRODUCT_DETAILS_SUCCESS:
             return { loading: false, product: action.payload }
         case PRODUCT_DETAILS_FAIL:
             return { loading: false, error: action.payload }
