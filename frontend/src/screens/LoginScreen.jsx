37bab6ebbc8242f71b3e5b8ddde65083e57fa544
Gabriel Gomes <gabrigomez15@gmail.com>
2021-02-08 18:00:28 -0300

    material ui in login

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,43 +1,126 @@
 import React, { useState } from 'react';
-import './LoginScreen.css'
+// import './LoginScreen.css'
 
-const Login = (props) => {
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+import lime from '@material-ui/core/colors/lime';
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+                Beer Store
+        </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(5),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
+
+export default function SignIn() {
+
+    const classes = useStyles()
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     return (
-
-        <div className="logIn">
-            <form>
-                <div className="loginField">
-                    Login
-                </div>
-                <div className="fields">
-                    <label htmlFor="email">E-mail  </label>
-                    <input
-                        type="text"
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign in
+                </Typography>
+                <form className={classes.form} noValidate>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
                         id="email"
-                        placeholder="Digite seu e-mail"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
                         required
-                        onChange={(e) => setEmail(e.target.value)} >
-                    </input>
-                </div>
-                <div className="fields">
-                    <label htmlFor="password">Senha  </label>
-                    <input
-                        type="text"
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
                         id="password"
-                        placeholder="Digite sua senha"
-                        required
-                        onChange={(e) => setEmail(e.target.value)} >
-                    </input>
-                </div>
-            </form>
-            <button className="button">
-                Entrar
-            </button>
-        </div>
-
+                        autoComplete="current-password"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <FormControlLabel
+                        control={<Checkbox value="remember" color="secondary" />}
+                        label="Remember me"
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="secondary"
+                        className={classes.submit}
+                    >
+                        Entrar
+                    </Button>
+                    <Grid container>
+                        <Grid item xs>
+                            <Link href="#" variant="body2">
+                                Esqueci a senha
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link href="#" variant="body2">
+                                {"Não possui conta? Cadastre-se aqui!"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
+            <Box mt={8}>
+                <Copyright />
+            </Box>
+        </Container>
     );
-};
-
-export default Login;
+}
