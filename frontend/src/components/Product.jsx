7d4ba2d0c42922298fe87c6171ff1e6a21c4c155
Gabriel Gomes <gabrigomez15@gmail.com>
2021-02-20 13:07:25 -0300

    change shadow in product details and mark add cart button to do in main page

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Product.css'
 
 import { Link } from 'react-router-dom'
@@ -6,6 +6,14 @@ import { Link } from 'react-router-dom'
 
 const Product = (props) => {
     const { product } = props
+
+    const [qtd, setQtd] = useState(1)
+
+
+    const addToCart = () => {
+        // props.history.push(`/cart/${productId}?qtd=${qtd}`)
+        // TODO
+    }
     return (
         <div>
             <div className="productContainer">
@@ -14,6 +22,10 @@ const Product = (props) => {
                     <div className="productNameInfo"> {product.name} </div>
                     <div className="productPriceInfo"> R${product.price} </div>
                 </Link>
+                {/* <button className='btn' onClick={addToCart}>
+                    ADICIONAR
+                        <i class="fas fa-shopping-cart"></i>
+                </button> */}
             </div>
         </div>
     );
